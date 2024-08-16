9a2b3b11a485f51bde5a77a05d6965db9767b8cf
Irina <il064330@gmail.com>
2024-08-16 23:04:34 +0300

    feat: create code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function toReadable (number) {
-  
+    const units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number === 0) {
+        return 'zero';
+    }
+
+    if (number < 20) {
+      return units[number];
+    }
+
+    if (number < 100) {
+        return `${tens[Math.floor(number / 10)]} ${units[number % 10]}`.trim();
+    }
+
+    if (number < 1000) {
+        if (number % 100 === 0) {
+            return `${units[Math.floor(number / 100)]} hundred`;
+        } else {
+            return `${units[Math.floor(number / 100)]} hundred ${toReadable(number % 100)}`.trim();
+        }
+    }
+
+    if (number >= 1000) {
+        return `${number} is too large to convert`;
+    }
+
 }
